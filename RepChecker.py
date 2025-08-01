ea93ecc9682c5dcbdb0027f0ab1ebdfa88b7b2c3
Eren Güven <121792966+RejectedFrASELS@users.noreply.github.com>
2025-08-01 18:26:05 +0300

    Add files via upload

--- RepChecker.py
+++ RepChecker.py
@@ -1,21 +1,21 @@
 import requests
 import json
 from datetime import datetime
-import urllib3
 import argparse
 import os
 import re
-
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 import sys
 from threading import Thread
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+load_dotenv()
 
 """
-VER 1.1.1
+VER 1.2
 """
 
-version = "1.1.1"
+version = "1.2"
 
 changeLog = """
 CHANGE LOG
@@ -27,6 +27,10 @@ CHANGE LOG
 == VER 1.1.1
 - Fixed issues in HTML file for AbuseIPDB reporting,
 - Added a text when a report is generated at the end of cli output.
+== VER 1.2
+- Added Country Codes from VT & AbuseIPDB
+- Added .env
+- Added optional Spur integration
 """
 
 print(f"""                                          
@@ -51,6 +55,7 @@ argParser.add_argument("-i", "--input", type=str, help="To check single value.",
 argParser.add_argument("-r", "--report", action="store_true", help="Generate HTML report.", required=False)
 argParser.add_argument("-vt", "--virustotal", action="store_true", help="Disable virustotal", required=False)
 argParser.add_argument("-ab", "--abuse", action="store_true", help="Disable AbuseIPDB", required=False)
+argParser.add_argument("-sp", "--spur", action="store_true", help="Enable Spur", required=False)
 argParser.add_argument("-vr", "--virustotalreports", type=int, help="Virustotal malicious reports threshold. Default 1.", required=False, default=1, metavar="[Threshold number]")
 argParser.add_argument("-ar", "--abusereports", type=int, help="Abuse IP DB malicious reports threshold. Default 1.", required=False, default=1, metavar="[Threshold number]")
 argParser.add_argument("-p", "--path", type=str, help="Provide path of the txt file contains targets. Default 'target-list.txt'.", required=False, default="target-list.txt")
@@ -87,6 +92,32 @@ def readFunc(filename):
             output.append(line.strip())
     return output
 
+def checkSpur(value):
+    try:
+        url = f"https://spur.us/context/{value}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
+        }
+
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            return "None/Error possible due to Rate Limit"
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        meta = soup.find("meta", attrs={"name": "description"})
+        if not meta or not meta.get("content"):
+            return "None/Error possible due to Rate Limit"
+
+        content = meta["content"]
+
+        # IP'den sonraki 4. noktaya kadar al
+        dots = [m.start() for m in re.finditer(r'\.', content)]
+        if len(dots) < 3:
+            return content.strip()
+        return content[:dots[3]+1].strip()
+    except:
+        return "None/Error possible due to Rate Limit"
+
 # ABUSE FUNC
 def checkAbuse(value, counter):
     try:
@@ -103,11 +134,11 @@ def checkAbuse(value, counter):
                 'Key': abuseApiKeys[counter % len(abuseApiKeys)]
             }
 
-            abuse_response = requests.request(method='GET', url=abuse_url, headers=abuse_headers, params=querystring,
-                                                    verify=False)
+            abuse_response = requests.request(method='GET', url=abuse_url, headers=abuse_headers, params=querystring)
             decodedResponse = json.loads(abuse_response.text)
 
             ispName = decodedResponse['data']['isp']
+            countryCodeAbuse = decodedResponse['data']['countryCode']
             reportScore = decodedResponse['data']['abuseConfidenceScore']
             reportCount = decodedResponse['data']['totalReports']
             usageType = decodedResponse['data']['usageType']
@@ -116,6 +147,7 @@ def checkAbuse(value, counter):
             reportCount, reportScore= (0, 0)
             returnJson = {
             "ispName" : "Can only check valid IP addresses on AbuseIPDB",
+            "countryCodeAbuse" : "Can only check valid IP addresses on AbuseIPDB",
             "reportScore" : 0,
             "reportCount" : 0,
             "usageType" : "Can only check valid IP addresses on AbuseIPDB",
@@ -125,6 +157,7 @@ def checkAbuse(value, counter):
 
         returnJson = {
             "ispName" : ispName,
+            "countryCodeAbuse" : countryCodeAbuse,
             "reportScore" : reportScore,
             "reportCount" : reportCount,
             "usageType" : usageType,
@@ -134,6 +167,7 @@ def checkAbuse(value, counter):
     except:
         returnJson = {
             "ispName" : "error",
+            "countryCodeAbuse" : "error",
             "reportScore" : 0,
             "reportCount" : 0,
             "usageType" : "error",
@@ -142,6 +176,8 @@ def checkAbuse(value, counter):
         return(returnJson)
 
 # VIRUS TOTAL FUNC
+import time  # Add this at the top if not already imported
+
 def checkVirustotal(value, counter):
     try:
         if ipPattern.search(value):
@@ -156,32 +192,41 @@ def checkVirustotal(value, counter):
             "x-apikey": virustotalApiKeys[counter % len(virustotalApiKeys)]
         }
 
-        response = requests.get(url, headers=headers, verify=False)
+        response = requests.get(url, headers=headers)
         json_file = json.loads(response.text)
 
         try:
             asOwner = json_file["data"]["attributes"]["as_owner"]
         except:
             asOwner = "None"
+        try:
+            countryCode = json_file["data"]["attributes"]["country"]
+        except:
+            countryCode = "None"
         lastAnalysisStats = json_file["data"]["attributes"]["last_analysis_stats"]
         isMalicious = json_file["data"]["attributes"]["last_analysis_stats"]["malicious"]
 
         returnJson = {
             "asOwner" : asOwner,
+            "countryCode" : countryCode,
             "lastAnalysisStats" : lastAnalysisStats,
             "isMalicious" : isMalicious,
             "error" : False
         }
-        
+
+        time.sleep(2)
         return(returnJson)
     except:
+        time.sleep(2)
         returnJson = {
             "asOwner" : "Error",
+            "countryCode" : "Error",
             "lastAnalysisStats" : {"Error": "Error with VirusTotal"},
             "isMalicious" : 0,
             "error" : True
         }
         return(returnJson)
+
 
 def checking(value, counter):
     global htmlReport 
@@ -196,11 +241,13 @@ def checking(value, counter):
         jsonVirustotal = checkVirustotal(address, ct)
         # VT VALUES
         asOwner = jsonVirustotal["asOwner"]
+        countryCode = jsonVirustotal["countryCode"]
         lastAnalysisStats = jsonVirustotal["lastAnalysisStats"]
         isMalicious = jsonVirustotal["isMalicious"]
         vtError = jsonVirustotal["error"]
         print("\n==Virus Total=="+ ("=" * 35) + "\nAddress:" + address)
         print(f"\tAS Owner: {asOwner}")
+        print(f"\tCountry Code: {countryCode}")
         print("\tLast Analysis Stats:")
         for engine, result in lastAnalysisStats.items():
             if isinstance(result, dict):
@@ -216,17 +263,21 @@ def checking(value, counter):
         jsonAbuse = checkAbuse(address, ct)
         # ABUSE VALUES
         ispName = jsonAbuse["ispName"]
+        countryCodeAbuse = jsonAbuse["countryCodeAbuse"]
         reportScore = jsonAbuse["reportScore"]
         reportCount = jsonAbuse["reportCount"]
         usageType = jsonAbuse["usageType"]
         abuseError = jsonAbuse["error"]
-        print("==Abuse IP DB==" + ("=" *35) + "\nAddress:" + address + "\n\tISP Name: " + ispName + "\n\tAbuse Score: " + str(
+        print("==Abuse IP DB==" + ("=" *35) + "\nAddress:" + address + "\n\tISP Name: " + ispName + "\n\tCountry Code: " + countryCodeAbuse + "\n\tAbuse Score: " + str(
                 reportScore) + "\n\tReport Counts: " + str(reportCount) + "\n\tUsage Type: " + str(usageType))
     else:
         reportScore = 0
         reportCount = 0
         abuseError = False
         usageType = "NotChecked"
+    if not args.spur:
+        spurText = checkSpur(address)
+        print("==Spur Report==" + ("=" *34) + "\nAddress:" + address + "\n\tSpur Comment: " + spurText )
 
     #print("=" * 50)
     
@@ -258,6 +309,10 @@ def checking(value, counter):
                             <td>{asOwner}</td>
                         </tr>
                         <tr>
+                            <td>Country Code:</td>
+                            <td>{countryCode}</td>
+                        </tr>
+                        <tr>
                             <td>Last Analysis Stats:</td>
                             <td>
                                 <table>
@@ -281,6 +336,10 @@ def checking(value, counter):
                             <td>ISP Name:</td>
                             <td>{ispName}</td>
                         </tr>
+                        <tr> 
+                            <td>Country Code:</td>
+                            <td>{countryCodeAbuse}</td>
+                        </tr>                        
                         <tr>
                             <td>Abuse Score:</td>
                             <td>{reportScore}</td>
@@ -296,6 +355,23 @@ def checking(value, counter):
                     '''
             startHtml = startHtml + abuseHtml
 
+        if not args.spur:    
+            spurHtml = f'''            
+                        <tr>
+                            <td colspan="2" align="center"><strong><a href="https://spur.us/context/{address}" target="_blank">Spur</a></strong></td>
+                        </tr>
+                        <tr>
+                            <td><b>Address:</b></td>
+                            <td><b>{address}</b></td>
+                        </tr>
+                        <tr> 
+                            <td>Info:</td>
+                            <td>{spurText}</td>
+                        </tr>
+                    '''
+            startHtml = startHtml + spurHtml
+
+
         startHtml = startHtml + endHtml
         htmlReport.append(startHtml)
     
@@ -304,9 +380,6 @@ def checking(value, counter):
     else:
         successfullyChecked = successfullyChecked + 1
     
-    #totalCounter = totalCounter + 1
-
-
 if __name__ == "__main__":
     
     # Control which APIs to use
@@ -314,9 +387,13 @@ if __name__ == "__main__":
         print("Not using any APIs, exitting...")
         exit() # Exit if user disabled both
     if args.virustotal is False:
-        virustotalApiKeys = list(set(readFunc('api-keys\\vt-apikeys.txt'))) # Get vt api keys
+        vt_keys_raw = os.getenv("VT_API_KEYS", "")
+        virustotalApiKeys = list(set(vt_keys_raw.strip().split(","))) if vt_keys_raw else []
+        #virustotalApiKeys = list(set(readFunc('api-keys/vt-apikeys.txt'))) # Get vt api keys
     if args.abuse is False:
-        abuseApiKeys = list(set(readFunc('api-keys\\abuse-apikeys.txt'))) # Get abuse api keys
+        #abuseApiKeys = list(set(readFunc('api-keys/abuse-apikeys.txt'))) # Get abuse api keys
+        abuse_keys_raw = os.getenv("ABUSE_API_KEYS", "")
+        abuseApiKeys = list(set(abuse_keys_raw.strip().split(","))) if abuse_keys_raw else []
 
     # INIT COUNTERS
 
